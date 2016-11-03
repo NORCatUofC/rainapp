8be9c339c75412be7bb9f6657180b4124310bb55
sbeslow <bezzy21@yahoo.com>
2016-11-02 23:35:41 -0500

    Add hourly precip to the database

--- events/models.py
+++ events/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class HourlyPrecip(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    precip = models.FloatField()
