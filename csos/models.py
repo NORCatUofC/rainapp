b702093cda999b9e6bde34295d7b8c5fd9f931e5
Scott Beslow <beslow-scott@norc.org>
2016-11-04 16:40:33 -0500

    Added lake reversal tables to database.  Added rows for three outfall points

--- csos/models.py
+++ csos/models.py
@@ -11,3 +11,16 @@ class RiverCso(models.Model):
     river_outfall = models.ForeignKey("RiverOutfall")
     open_time = models.DateTimeField()
     close_time = models.DateTimeField()
+
+
+class LakeOutfall(models.Model):
+    name = models.TextField()
+    lat = models.FloatField(null=True)
+    lon = models.FloatField(null=True)
+
+
+class LakeReversal(models.Model):
+    lake_outfall = models.ForeignKey("LakeOutfall")
+    open_date = models.DateField()
+    close_date = models.DateField()
+    millions_of_gallons = models.FloatField()
