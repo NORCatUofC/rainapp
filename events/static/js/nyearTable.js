9f32319e6360e86f18cb1dbe7308bcb10108daff
Scott Beslow <beslow-scott@norc.org>
2016-11-22 18:03:10 -0600

    Modified nyear table to look better

--- events/static/js/nyearTable.js
+++ events/static/js/nyearTable.js
@@ -4,15 +4,36 @@ function setupNYearTable(nyearTableArg) {
     drawNYearTable(100);
 }
 
-function drawNYearTable(n) {
+function drawNYearTable(n, drawFull) {
     $("#nyearTable").empty();
+    $("#placeForPaginationBtn").empty();
     var events = nyearTable[n];
-    var tableStr = ""
-    for (var i = 0; i < events.length; i++) {
+    var tableStr = "";
+    var numToShow = events.length;
+    const MAX_TO_SHOW = 7;
+    $("#nYearStormSelected").text(n);
+    if (events.length > MAX_TO_SHOW) {
+        if (drawFull) {
+            var r = $('<button type="button" style="text-align:center" class="btn btn-primary" onclick="drawTableAndMoveUp(' + n + ', false)">See Less</button>');
+            $("#placeForPaginationBtn").append(r);
+        }
+        else {
+            numToShow = MAX_TO_SHOW;
+            var r = $('<button type="button" style="text-align:center" class="btn btn-primary" onclick="drawNYearTable(' + n + ', true)">See More</button>');
+            $("#placeForPaginationBtn").append(r);
+        }
+    }
+    for (var i = 0; i < numToShow; i++) {
         var event = events[i];
         tableStr += "<tr><td>" + event.date_formatted + "</td>" +
-                "<td>" + event.inches + " inches in " + event.duration_formatted + "</td>" +
-                "<td><a href='" + event.event_url + "' class='btn btn-primary btn-xs'>See it</a></td></tr>"
+            "<td>" + event.inches + " inches in " + event.duration_formatted + "</td>" +
+            "<td><a href='" + event.event_url + "' class='btn btn-primary btn-xs'>See it</a></td></tr>"
     }
     $('#nyearTable').append(tableStr);
+
+}
+
+function drawTableAndMoveUp(n, drawFull) {
+    drawNYearTable(n, drawFull);
+    $("body, html").scrollTop($("#nYearStormSelected").offset().top);
 }
